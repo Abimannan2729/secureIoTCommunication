db755ab6681056f14fef694f5982cd275ba74307
Abimannan M <abimannan2729@outlook.com>
2025-10-08 09:44:29 +0530

    Update lambda_function.py

--- DecryptDHT22Payload-2f107cac-926e-4ea6-ad30-27d694ba21b0/lambda_function.py
+++ DecryptDHT22Payload-2f107cac-926e-4ea6-ad30-27d694ba21b0/lambda_function.py
@@ -15,11 +15,11 @@ AES_IV = bytes([
     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
     0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
 ])
-HMAC_KEY = b"S3cUr3!HMACk3y1234"
+HMAC_KEY = b"enter hmac same as your .ino code file"
 
 # ---------------- DynamoDB ----------------
 dynamodb = boto3.resource('dynamodb')
-TABLE_NAME = "ESP32_DHT22_Data_New"
+TABLE_NAME = "enter table name here"
 table = dynamodb.Table(TABLE_NAME)
 
 # ---------------- Helper Functions ----------------
